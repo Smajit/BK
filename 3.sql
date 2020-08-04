c7ddb9896779b19fbcd998b2211d4b76cf86e488
Smajit <69045997+Smajit@users.noreply.github.com>
2020-08-04 18:31:48 +0530

    Create 3.sql

---
+++ 3.sql
@@ -0,0 +1,8 @@
+SELECT id, name, marks,
+CASE marks
+WHEN marks > 90 THEN 'A+'
+WHEN marks > 70 THEN 'A'
+WHEN marks > 50 THEN 'B'
+WHEN marks >=40 THEN 'C'
+ELSE 'Fail'
+END AS grade FROM students;
