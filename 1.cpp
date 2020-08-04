5a5df8d25eaf37b3efe7f765d4195de7781bfdc8
Smajit <69045997+Smajit@users.noreply.github.com>
2020-08-04 21:06:19 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,13 +1,34 @@
-boolean isPallindrome(char str[])
+
+#include <iostream>
+#include <string.h>
+
+
+
+bool isPallindrome(char[]);
+
+int main()
+{
+    char str[100]="1881";
+    std::cout<<isPallindrome(str)<<std::endl;
+    strcpy(str,"Dad");
+    std::cout<<isPallindrome(str)<<std::endl;
+    strcpy(str,"Step on no pets");
+    std::cout<<isPallindrome(str)<<std::endl;
+    return 0;
+}
+
+bool isPallindrome(char str[])
 {
   int i = 0;
   int j = strlen(str) - 1;
   
   while(j>i)
   {
-    if(str[i++] != str[j--])
-      return False;
+    char LHS = toupper(str[i++]); 
+    char RHS = toupper(str[j--]);
+    if(LHS != RHS)
+      return false;
    
   }
-  return True;
+  return true;
 }
